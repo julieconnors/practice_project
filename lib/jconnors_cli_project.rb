97c0a7c3053e8098723d65b1cfc49d36b1a38ec8
Juliana Connors <juliana.g.connors@gmail.com>
2020-08-11 22:35:26 +0000

    initial setup

---
+++ lib/jconnors_cli_project.rb
@@ -0,0 +1,6 @@
+require "jconnors_cli_project/version"
+
+module JconnorsCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jconnors_cli_project/version.rb
@@ -0,0 +1,3 @@
+module JconnorsCliProject
+  VERSION = "0.1.0"
+end
